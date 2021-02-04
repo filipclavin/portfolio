edeffb0809f06bab5e0b16ce9d3b339e10969f19
filipclavin <filip.clavin@student.kyh.se>
2021-02-04 12:18:13 +0100

    set up react routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
+import Navbar from "./Navbar";
 
 function App() {
   return (
-    <>
-      <h1>Filip Clavin</h1>
-    </>
+    <Router>
+      <header>
+        <h1>Filip Clavin</h1>
+        <Navbar />
+      </header>
+
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
